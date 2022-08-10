1d952e381d4b26e2a2bb6fe3065bf3860da9345c
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-08-10 15:52:21 +0900

    working

--- download.sh
+++ download.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-. $(dirname $0)/path.sh
+
+VERSION=$(head -1 debian/changelog | sed 's/1://g' | cut -d ' ' -f 2 | sed 's/[()]//g')
+VERSION_BASE=$(echo ${VERSION} | cut -d '-' -f 1)
 
 if [ -f espresso_$VERSION_BASE.orig.tar.gz ]; then
   exit 127
 fi
-wget https://github.com/QEF/q-e/releases/download/qe-$VERSION_BASE.0/qe-$VERSION_BASE-ReleasePack.tgz
+wget https://github.com/QEF/q-e/releases/download/qe-$VERSION_BASE/qe-$VERSION_BASE-ReleasePack.tgz
 tar zxvf qe-$VERSION_BASE-ReleasePack.tgz
 mv -f qe-$VERSION_BASE espresso-$VERSION_BASE
-tar zcvf espresso-$VERSION_BASE.tar.gz espresso-$VERSION_BASE
+tar zcvf espresso_$VERSION_BASE.orig.tar.gz espresso-$VERSION_BASE
 rm -rf espresso-$VERSION_BASE qe-$VERSION_BASE-ReleasePack.tgz
 
--- host-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. $(dirname $0)/path.sh
-
-VM="madev10-amd64 madev9-amd64 ubuntu20-amd64 ubuntu18-amd64"
-for v in $VM; do
-  cd $HOME/vagrant/$v
-  vagrant ssh -c "sh malive/development/ma-${PACKAGE}/setup.sh"
-  vagrant ssh -c "sh malive/development/ma-${PACKAGE}/build.sh"
-done
