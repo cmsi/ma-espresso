0de45e60ed6b59f4f2ca2bcd1251d7b88ca3f105
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-08-16 15:09:44 +0900

    update

--- download.sh
+++ download.sh
@@ -10,5 +10,6 @@ rm -f qe-$VERSION_BASE.tar.gz*
 curl -L -O https://github.com/QEF/q-e/archive/refs/tags/qe-$VERSION_BASE.tar.gz
 tar zxvf qe-$VERSION_BASE.tar.gz
 mv -f q-e-qe-$VERSION_BASE espresso-$VERSION_BASE
+(cd espresso-$VERSION_BASE/Doc && latexmk -pdf user_guide.tex && latexmk -pdf brillouin_zones.tex && latexmk -pdf constraints_HOWTO.tex && latexmk -pdf plumed_quick_ref.tex && latexmk -c)
 tar zcvf espresso_$VERSION_BASE.orig.tar.gz espresso-$VERSION_BASE
 rm -rf espresso-$VERSION_BASE qe-$VERSION_BASE.tar.gz
