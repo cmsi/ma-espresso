397ff7044bf4b2a0b57fd2106da3ae032d661b5c
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-08-16 14:33:21 +0900

    udpate

--- download.sh
+++ download.sh
@@ -6,9 +6,9 @@ VERSION_BASE=$(echo ${VERSION} | cut -d '-' -f 1)
 if [ -f espresso_$VERSION_BASE.orig.tar.gz ]; then
   exit 127
 fi
-wget https://github.com/QEF/q-e/releases/download/qe-$VERSION_BASE/qe-$VERSION_BASE-ReleasePack.tgz
-tar zxvf qe-$VERSION_BASE-ReleasePack.tgz
-mv -f qe-$VERSION_BASE espresso-$VERSION_BASE
+rm -f qe-$VERSION_BASE.tar.gz*
+curl -L -O https://github.com/QEF/q-e/archive/refs/tags/qe-$VERSION_BASE.tar.gz
+tar zxvf qe-$VERSION_BASE.tar.gz
+mv -f q-e-qe-$VERSION_BASE espresso-$VERSION_BASE
 tar zcvf espresso_$VERSION_BASE.orig.tar.gz espresso-$VERSION_BASE
-rm -rf espresso-$VERSION_BASE qe-$VERSION_BASE-ReleasePack.tgz
-
+rm -rf espresso-$VERSION_BASE qe-$VERSION_BASE.tar.gz
